6db70c586dfb8d7011a9c2bfc530754a7ea0283f
Syed Aziz <syedaziz27x@gmail.com>
2019-03-22 21:07:40 -0400

    added heroku remote

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const {app,} = require('./app')
+const {app,} = require('./app');
 
-app.listen(3001, () => {
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
     console.log('listening on port: 3001')
 });
