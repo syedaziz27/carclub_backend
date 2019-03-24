b6b4164acca4eed07f8bd6b34a5621ae9bd3b7ab
Syed Aziz <syedaziz27x@gmail.com>
2019-03-24 16:00:42 -0400

    updated dbconnect url{

--- services/dbConnect.js
+++ services/dbConnect.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')({});
-const db = pgp(('process.env.DATABASE_URL || postgress://localhost/carclub1'));
+const db = pgp(process.env.DATABASE_URL || 'postgress://localhost/carclub1');
 
 module.exports = {
     db,
